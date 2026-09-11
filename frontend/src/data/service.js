fcaf96f2935a6d63fdc88952934598199081390a
abinayaj-coder <abinaya.zerosoft@gmail.com>
2026-09-11 13:07:45 +0530

    url change

--- frontend/src/data/service.js
+++ frontend/src/data/service.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.REACT_APP_API_URL || 'https://lms-orpin-gamma.vercel.app/';
+export const API_URL = process.env.REACT_APP_API_URL || 'https://lms.yencodetechnologies.in/';
