912082c40e225f04d656d7c8b848622fa80b0559
abinayaj-coder <abinaya.zerosoft@gmail.com>
2026-09-12 14:22:23 +0530

    url update

--- frontend/src/data/service.js
+++ frontend/src/data/service.js
@@ -1,4 +1,3 @@
 // export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:2034';
 
 export const API_URL = process.env.REACT_APP_API_URL || 'https://lms.yencodetechnologies.in/';
-https://lms.yencodetechnologies.in/
